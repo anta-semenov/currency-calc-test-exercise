18a15fbe4d918dfa066d41169054dc3f4079d48a
Anton Semenov <anton@anton.local>
2016-08-11 17:12:22 +0500

    add terms and exchangeRates to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,32 @@
 import {combineReducers} from 'redux'
 import currencies, * as fromCurrencies from './currencies'
+import exchangeRates, * as fromExchangeRates from './exchangeRates'
+import terms, * as fromTerms from './terms'
+
 
 const reducer = combineReducers(
-  currencies
+  currencies,
+  exchangeRates,
+  terms
 )
 
 export default reducer
 
 export const getD3ResultGraphStackInput = (state) => {
   const temp = {}
-  const currenciesIds = fromCurrencies.currenciesIds(state.currencies)
-  state.termsHelper.forEach(termItem => {
-    const {termId, investRateMult} = termItem
+  const currenciesIds = fromCurrencies.getCurrenciesIds(state.currencies)
+  fromTerms.getTermsForResults(state.terms).forEach(termItem => {
+    const {termId, investRateMultiplicator} = termItem
     currenciesIds.forEach(currencyId => {
       if (!temp[termId]) {
         temp[termId] = {term: termId}
       }
       const {initialAmount, investRate} = state.currencies[currencyId]
-      temp[termId][currencyId] = initialAmount*(state.useInvest ? 1+(investRateMult*investRate)/100 : 1)*state.exchangeRates[''+termId+currencyId]
+      temp[termId][currencyId] = initialAmount*(state.useInvest ? 1+(investRateMultiplicator*investRate)/100 : 1)*fromExchangeRates.getAllExchangeRates(state.exchangeRates)[''+termId+currencyId]
     })
   })
+
+  const result = []
+  Object.keys(temp).forEach(key => result.push(temp[key]))
+  return result
 }
