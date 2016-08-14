a9a6427556584100a76ff07e0e4ffb1c3612f1b4
Anton Semenov <anton@anton.local>
2016-08-14 18:26:50 +0500

    add exRates past reducer tests

---
+++ tests/reducers/past_spec.js
@@ -0,0 +1,184 @@
+import {expect} from 'chai'
+import past, * as fromPast from '../../src/reducers/exchangeRates/past'
+import {addCurrencyResult} from '../../src/actions/actionCreator'
+
+describe('Past reducer & selectors', () => {
+  it('Should return correct currentRates', () => {
+    const testPastState = {
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.58,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.78,
+        isInitial: true
+      }
+    }
+    const expectedCurrentRates = {
+      'EUR': 0.87,
+      'GBP': 1.78
+    }
+    expect(fromPast.getInitialRates(testPastState)).to.deep.equal(expectedCurrentRates)
+  })
+
+  it('Should handle ADD_CURRENCY', () => {
+    const initialState = {
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.58,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.78,
+        isInitial: true
+      }
+    }
+
+    const action = addCurrencyResult({}, {
+      ['' + new Date(2016,2,4).getTime() + 'CHF']: {
+        currencyId: 'CHF',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.02,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'CHF']: {
+        currencyId: 'CHF',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.05,
+        isInitial: true
+      }
+    }, {})
+
+    const expectedState = {
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.58,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.78,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'CHF']: {
+        currencyId: 'CHF',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.02,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'CHF']: {
+        currencyId: 'CHF',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.05,
+        isInitial: true
+      }
+    }
+
+    expect(past(initialState, action)).to.deep.equal(expectedState)
+  })
+
+  it('Should handle ADD_CURRENCY with emty past argument', () => {
+    const initialState = {
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.58,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.78,
+        isInitial: true
+      }
+    }
+
+    const action = addCurrencyResult({}, {}, {})
+
+    const expectedState = {
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87,
+        isInitial: true
+      },
+      ['' + new Date(2016,2,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,2,4).getTime(),
+        rate: 1.58,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'GBP']: {
+        currencyId: 'GBP',
+        term: new Date(2016,3,4).getTime(),
+        rate: 1.78,
+        isInitial: true
+      }
+    }
+
+    expect(past(initialState, action)).to.deep.equal(expectedState)
+  })
+})
