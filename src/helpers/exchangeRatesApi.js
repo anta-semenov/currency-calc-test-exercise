0d4df2f346c221d02cdd0dc37327c961cbce4eb0
Anton Semenov <anton@anton.local>
2016-08-14 18:26:33 +0500

    add exchangeRatesApi tests

---
+++ src/helpers/exchangeRatesApi.js
@@ -0,0 +1,83 @@
+/*global Promise*/
+import XMLHttpRequest from 'xhr2'
+import {round} from './core'
+
+const baseURL = 'http://api.fixer.io/'
+
+const getExchangeRate = (date, currencies, baseCurrency = 'USD') => new Promise((resolve, reject) => {
+  let url = baseURL
+  let tempDate = date
+  if (date instanceof Date) {
+    tempDate = date
+  } else {
+    const offset = new Date().getTimezoneOffset()
+    tempDate = new Date(date - offset*60000)
+  }
+  url += tempDate.toISOString().substring(0, 10)
+
+  url += '?symbols=' + currencies.toString() + '&base=' + baseCurrency
+  //send request
+  const request = new XMLHttpRequest()
+  request.onreadystatechange = () => {
+    if (request.readyState === XMLHttpRequest.DONE) {
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText)['rates']
+        const result = Object.keys(response).map((key) => {
+          return {
+            currency: key,
+            term: date instanceof Date ? date.getTime() : date,
+            rate: response[key]
+          }
+        })
+        resolve(result)
+      } else {
+        reject({})
+      }
+    }
+  }
+  request.open('GET', url, true)
+  request.send(null)
+})
+
+export const getPastExchangeRates = (pastTerms, currentTerm, currencies, baseCurrency) => {
+  const pastRequest = pastTerms.map(term =>
+    getExchangeRate(term, currencies, baseCurrency)
+  )
+
+  const currentRequest = getExchangeRate(currentTerm, currencies, baseCurrency)
+  return Promise.all([...pastRequest, currentRequest]).then(
+    response => {
+      const past = {}
+      response.forEach(exchangeRateResult => {
+        exchangeRateResult.forEach(currencyRate => {
+          //fullfill past rates
+          past['' + currencyRate.term + currencyRate.currency] = {
+            term: currencyRate.term,
+            currencyId: currencyRate.currency,
+            rate: currencyRate.rate,
+            isInitial: currencyRate.term === currentTerm ? true : undefined
+          }
+        })
+      })
+      return past
+    },
+    error => {
+      return error
+    }
+  )
+}
+
+export const getFutureExchangeRates = (futureTerms, currentRates) => {
+  const future = {}
+  const currenciesIds = Object.keys(currentRates)
+  futureTerms.forEach((term, index) => {
+    currenciesIds.forEach(currencyId => {
+      future['' + term + currencyId] = {
+        term,
+        currencyId,
+        rate: round(currentRates[currencyId]*(1+0.05*(index+1)), 4)
+      }
+    })
+  })
+  return future
+}
---
+++ tests/helpers/exchangeRatesApi_spec.js
@@ -0,0 +1,75 @@
+import {expect} from 'chai'
+import {getPastExchangeRates, getFutureExchangeRates} from '../../src/helpers/exchangeRatesApi'
+
+describe('exchange rates api', () => {
+  it('Should return correct past exchange rates dictionary', () => {
+    const pastTerms = [
+      new Date(2016,0,4).getTime(),
+      new Date(2016,1,4).getTime(),
+      new Date(2016,2,4).getTime()
+    ]
+    const currentTerm = new Date(2016,3,4).getTime()
+
+    const expectedResponse = {
+      ['' + new Date(2016,0,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,0,4).getTime(),
+        rate: 0.9176,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,1,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,1,4).getTime(),
+        rate: 0.89238,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,2,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,2,4).getTime(),
+        rate: 0.91158,
+        isInitial: undefined
+      },
+      ['' + new Date(2016,3,4).getTime() + 'EUR']: {
+        currencyId: 'EUR',
+        term: new Date(2016,3,4).getTime(),
+        rate: 0.87873,
+        isInitial: true
+      }
+    }
+
+    return getPastExchangeRates(pastTerms, currentTerm, ['EUR'], 'USD').then(
+      response => {
+        expect(response).to.deep.equal(expectedResponse)
+      }
+    )
+  })
+
+  it('Should return correct future rates', () => {
+    const futureTerms = [
+      new Date(2016,9,4).getTime(),
+      new Date(2016,10,4).getTime(),
+      new Date(2016,11,4).getTime()
+    ]
+    const currentRates = {
+      EUR: 0.8787
+    }
+    const expectedFutureRates = {
+      ['' + new Date(2016,9,4).getTime() + 'EUR']: {
+        term: new Date(2016,9,4).getTime(),
+        currencyId: 'EUR',
+        rate: 0.9226
+      },
+      ['' + new Date(2016,10,4).getTime() + 'EUR']: {
+        term: new Date(2016,10,4).getTime(),
+        currencyId: 'EUR',
+        rate: 0.9666
+      },
+      ['' + new Date(2016,11,4).getTime() + 'EUR']: {
+        term: new Date(2016,11,4).getTime(),
+        currencyId: 'EUR',
+        rate: 1.0105
+      }
+    }
+    expect(getFutureExchangeRates(futureTerms, currentRates)).to.deep.equal(expectedFutureRates)
+  })
+})
