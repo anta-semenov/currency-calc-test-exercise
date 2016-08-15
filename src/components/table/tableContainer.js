9c2faa0825c9039119eea0bfa09da43f1cfb24e7
Anton Semenov <anton@anton.local>
2016-08-15 17:05:21 +0500

    finished with tableContainer

--- src/components/table/tableContainer.js
+++ src/components/table/tableContainer.js
@@ -1,15 +1,43 @@
 import * as fromReducer from '../../reducers'
 import Table from './table'
 import {connect} from 'react-redux'
-import {openAddCurrencyDialog} from '../../actions.actionCreator'
+import * as actions from '../../actions.actionCreator'
 
 const mapStateToProps = state => ({
   currencies: fromReducer.getCurrenciesForTable(state),
-  currenciesForAdding: fromReducer.getCurrenciesForAdding(state)
+  currenciesForAdding: fromReducer.getCurrenciesForAdding(state),
+
+  pastTerms: fromReducer.getPastTerms(state),
+  futureTerms: fromReducer.getResultTerms(state),
+  currentTerm: fromReducer.getCurrentTerm(state),
+  userCurrency: fromReducer.getUserCurrency(state)
 })
 
 const mapDispatchToProps = dispatch => ({
-  addCurrency: () => dispatch(openAddCurrencyDialog())
+  addCurrency: (currency, userCurrency, pastTerms, futureTerms, currentTerm) => dispatch(actions.addCurrency(
+    currency,
+    userCurrency,
+    pastTerms,
+    futureTerms,
+    currentTerm
+  )),
+  changeAmount: (currencyId, newAmount) => dispatch(actions.changeCurrencyAmount(currencyId, newAmount)),
+  changeInvestRate: (currencyId, newInvestRate) => dispatch(actions.changeInvestRate(currencyId, newInvestRate))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Table)
+const mergeProps = (stateProps, dispatchProps, ownProps) => ({
+  ...ownProps,
+  currencies: stateProps.currencies,
+  currenciesForAdding: stateProps.currenciesForAdding,
+  changeAmount: dispatchProps.changeAmount,
+  changeInvestRate: dispatchProps.changeInvestRate,
+  addCurrency: currency => dispatchProps.addCurrency(
+    currency,
+    stateProps.userCurrency,
+    stateProps.pastTerms,
+    stateProps.futureTerms,
+    stateProps.currentTerm
+  )
+})
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(Table)
