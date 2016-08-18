039b3261bb4eff79b8bf725b38c1aabd7551d973
Anton Semenov <anton@anton.local>
2016-08-18 09:19:32 +0500

    add userCurrency menu to header

--- src/components/header/userCurrency/UserCurrency.js
+++ src/components/header/userCurrency/UserCurrency.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './UserCurrency.less'
+import AppearingMenu from '../../appearingMenu/AppearingMenu'
+import AddCurrencyButton from '../../table/addCurrencyButton/AddCurrencyButton'
 
-const UserCurrency = (props) => (
-  <div>
-    
-  </div>
-)
+const UserCurrency = ({userCurrency, availableCurrencies, changeUserCurrency}) => {
+  const items = availableCurrencies.map(item => (
+    <AddCurrencyButton {...item} addCurrency={changeUserCurrency} key={item.currencyId} />
+  ))
+  const labelComponent = <div>{userCurrency.label}</div>
+  return(
+    <div>
+      <AppearingMenu labelComponent={labelComponent} menuItems={items} direction='vertical'/>
+    </div>
+  )
+}
 
 UserCurrency.propTypes = {
-
+  userCurrency: React.PropTypes.object.isRequired,
+  availableCurrencies: React.PropTypes.array.isRequired,
+  changeUserCurrency: React.PropTypes.func.isRequired
 }
 
 export default UserCurrency
