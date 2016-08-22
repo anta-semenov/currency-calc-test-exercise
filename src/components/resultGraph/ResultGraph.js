a58f6f17666b97d7cf32c6d5b7ed45be8a748f58
Anton Semenov <anton@anton.local>
2016-08-22 10:39:56 +0500

    add animation to result graph

--- src/components/resultGraph/ResultGraph.js
+++ src/components/resultGraph/ResultGraph.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import './ResultGraph.less'
 import * as d3 from 'd3'
+import ResultGraphRect from './ResultGraphRect'
+import floor from 'lodash/floor'
 
 const height=200
 const width=250
 
 const ResultGraph = ({results, maxValue, colors, currenciesIds}) => {
   const scaleY = d3.scaleLinear().domain([0, maxValue*1.5]).range([0, height-30])
-  const scaleX = d3.scaleLinear().domain([0, 4]).range([0, width-30])
+  const scaleX = d3.scaleLinear().domain([0, 4]).range([0, width-40])
   const series = d3.stack().keys(currenciesIds)(results)
   const rects = []
   series.forEach((seria, index) => {
     seria.forEach((item, itemIndex) => {
       rects.push(
-        <rect key={index*10+itemIndex} fill={colors[index]} x={`${scaleX(itemIndex)+10}`} y={`${height-scaleY(item[1])-20}`} width={`${scaleX(1)-1}`} height={`${scaleY(item[1]-item[0])}`} />
+        <ResultGraphRect key={index*10+itemIndex} color={colors[index]} x={`${floor(scaleX(itemIndex), 1)+40}`} y={height-floor(scaleY(item[1]), 1)-20} width={`${floor(scaleX(1), 1)-1}`} height={floor(scaleY(item[1]-item[0]), 1)} />
       )
     })
   })
+  const axisY = d3.axisLeft(d3.scaleLinear().domain([maxValue*1.5, 0]).range([0, height-30])).ticks(4, 's')
+
+  const drawAxis = context => {
+    d3.select(context)
+      .attr('transform', 'translate(38,10)')
+      .call(axisY)
+  }
 
   return(
     <div className='result'>
-      <svg className='result-svg' viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+      <svg className='result-svg' viewBox={`0 0 ${width} ${height}`} width={width} height={height} >
+        <g x='0' y='0' width='20' height={`${height}`} ref={ref => {drawAxis(ref)}}/>
         {rects}
       </svg>
     </div>
---
+++ src/components/resultGraph/ResultGraphRect.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Motion, spring} from 'react-motion'
+
+const springConfig = {
+  stiffness: 260,
+  damping: 35,
+  precision: 0.1
+}
+
+const ResultGraphRect = ({height, width, x, y, color}) => (
+  <Motion style={{rectHeight: spring(height, springConfig), rectY: spring(y, springConfig)}}>
+    {({rectHeight, rectY}) =>
+      <rect
+        fill={color}
+        x={x}
+        y={rectY}
+        width={width}
+        height={rectHeight}
+      />
+    }
+  </Motion>
+)
+
+export default ResultGraphRect
+
+ResultGraphRect.propTypes = {
+  height: React.PropTypes.string.isRequired,
+  width: React.PropTypes.string.isRequired,
+  x: React.PropTypes.string.isRequired,
+  y: React.PropTypes.string.isRequired,
+  color: React.PropTypes.string.isRequired
+}
