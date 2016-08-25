7f1e7d949efd1d9863a4a7aa5038b2af5191bf32
Anton Semenov <anton@anton.local>
2016-08-25 20:42:59 +0500

    minor updates

--- src/components/rateGraph/RatesLines.js
+++ src/components/rateGraph/RatesLines.js
@@ -5,16 +5,18 @@ import * as d3 from 'd3'
 const RatesLines = ({rates, colors, scaleX, scaleY}) => {
   const pathRateLines = Object.keys(rates).map(item => {
     const path = d3.path()
+    let key = item
     rates[item].forEach((termRateItem, index) => {
       if (index === 0) {
         path.moveTo(scaleX(termRateItem.term), scaleY(termRateItem.rate))
+        key += termRateItem.term
       } else {
         path.lineTo(scaleX(termRateItem.term), scaleY(termRateItem.rate))
       }
     })
 
     return(
-      <path d={path.toString()} stroke={colors[item]} fill='transparent' />
+      <path d={path.toString()} stroke={colors[item]} fill='transparent' key={key} />
     )
   })
 
