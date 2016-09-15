3119d58a3f61293eb603f521e5b2a48d77a0d085
Anton Semenov <anton@anton.local>
2016-09-15 14:53:52 +0500

    delete unused actions and action types

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -6,10 +6,6 @@ import {getPastExchangeRates, getFutureExchangeRates} from '../helpers/exchangeR
 
 export const setState = state => ({type: actionTypes.SET_STATE, state})
 
-export const changeCircleCoord = (y) => ({type: actionTypes.CHANGE_CIRCLE_COORD, y:y})
-export const startExRateChanging = (id, x) => ({type: actionTypes.START_EXCHANGE_RATE_CHANGING, id: id, x: x})
-export const stopExRateChanging = () => ({type: actionTypes.STOP_EXCHANGE_RATE_CHANGING})
-
 export const changeCurrencyAmount = (currencyId, newAmount) => ({type: actionTypes.CHANGE_CURRENCY_AMOUNT, currencyId, newAmount})
 export const changeInvestRate = (currencyId, newInvestRate) => ({type: actionTypes.CHANGE_CURRENCY_INVEST_RATE, currencyId, newInvestRate})
 export const changeExchangeRate = (currencyId, term, newRate) => ({type: actionTypes.CHANGE_CURRENCY_EXCHANGE_RATE, currencyId, term, newRate})
